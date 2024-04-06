5cf7c712868a6d6518c321909ce70b7a7bee2090
Tornaco <Tornaco@163.com>
2024-04-06 17:48:01 +0800

    add some intro and check

--- app/src/main/java/tornaco/apps/shortx/ext/MainActivity.kt
+++ app/src/main/java/tornaco/apps/shortx/ext/MainActivity.kt
@@ -19,14 +19,17 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import tornaco.apps.shortx.core.res.Remix
 import tornaco.apps.shortx.core.shortXManager
 import tornaco.apps.shortx.ext.shortcut.ExportDAShortcut
-import tornaco.apps.shortx.ui.base.ErrorCard
 import tornaco.apps.shortx.ui.base.RemixIcon
 import tornaco.apps.shortx.ui.base.ShortXMediumTopAppBar
+import tornaco.apps.shortx.ui.base.TipCard
+import tornaco.apps.shortx.ui.base.TipDialog
+import tornaco.apps.shortx.ui.base.rememberTipDialogState
 import tornaco.apps.shortx.ui.theme.ShortXTheme
 
 @AndroidEntryPoint
@@ -48,22 +51,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
+    val appIntroDialog =
+        rememberTipDialogState(title = "About", tip = stringResource(id = R.string.app_intro))
+    TipDialog(state = appIntroDialog)
+
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             ShortXMediumTopAppBar(
                 modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-                title = { Text(text = "Extensions") },
+                title = { Text(text = stringResource(id = R.string.app_name)) },
                 navigationIcon = {
-                    IconButton(onClick = {
-
-                    }) {
-                        RemixIcon(remixName = Remix.System.close_line)
-                    }
                 },
                 actions = {
-
+                    IconButton(onClick = { appIntroDialog.show() }) {
+                        RemixIcon(remixName = Remix.System.information_line)
+                    }
                 },
                 scrollBehavior = scrollBehavior
             )
@@ -74,12 +78,23 @@ fun MainContent() {
                 .padding(paddingValues)
         ) {
             AnimatedVisibility(visible = !shortXManager.isInstalled) {
-                ErrorCard(
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    title = "ShortX is not active",
-                    warnings = emptyList()
+                TipCard(
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .padding(horizontal = 16.dp),
+                    tip = "ShortX is not active",
                 )
             }
+
+            AnimatedVisibility(visible = shortXManager.isInstalled && shortXManager.version().code < 102051) {
+                TipCard(
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .padding(horizontal = 16.dp),
+                    tip = "The installed ShortX version is too low. Please update it.",
+                )
+            }
+
             ExportDAShortcut()
         }
     }
